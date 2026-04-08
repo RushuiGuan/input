b6869636359d2a45c0b5777ed96d300ff02ed56a
rushuiguan <rushui@gmail.com>
2026-04-07 23:01:00 -0400

    remove santize

--- Albatross.Input/Extensions.cs
+++ Albatross.Input/Extensions.cs
@@ -2,9 +2,8 @@ using FluentValidation.Results;
 
 namespace Albatross.Input {
 	public static class Extensions {
-		public static ValidationResult Validate<T>(this T request, out T sanitized) where T : class, IRequest<T> {
-			sanitized = request.Sanitize();
-			return T.Validator.Validate(sanitized);
+		public static ValidationResult Validate<T>(this T request) where T : class, IRequest<T> {
+			return T.Validator.Validate(request);
 		}
 	}
 }
--- Albatross.Input/IRequest.cs
+++ Albatross.Input/IRequest.cs
@@ -2,7 +2,6 @@
 
 namespace Albatross.Input {
 	public interface IRequest<T> where T : class, IRequest<T> {
-		T Sanitize();
 		static abstract AbstractValidator<T> Validator { get; }
 	}
 }
